3c1f7e2c10f7bb27bd448959f08f355ddb0ea565
ltai <ltai@math.fsu.edu>
2017-04-24 15:42:21 -0400

    Add files via upload

---
+++ my_import.m
@@ -0,0 +1,16 @@
+% must run this routine before everything
+% created at May, 28, 2014
+
+TrendBase = pwd;               
+addpath([TrendBase '/SRVFs']); 
+addpath([TrendBase '/Routines']); 
+addpath([TrendBase '/Basis']); 
+addpath([TrendBase '/Data']); 
+addpath([TrendBase '/Models']); 
+addpath([TrendBase '/Plot_Results']); 
+
+addpath([TrendBase '/Tests']); 
+%addpath([TrendBase '/Success_Code']);
+
+load MyColors;
+load MyMarkers;
